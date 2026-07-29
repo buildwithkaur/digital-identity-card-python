70f5fa7e83d20f5de32cf55529757ccabdafb1a2
buildwithkaur <tarankaur1146@gmail.com>
2026-07-29 19:29:21 +0530

    Add Digital Identity Card Project

---
+++ 03_digital_identity_card.py
@@ -0,0 +1,21 @@
+# Digital ID Card
+# Created By : Taranjeet Kaur
+# Mission 03 - Python Variables Project 
+name = "ABC"
+age = 19
+branch = "Computer Science and Engineering"
+city = "Delhi"
+dream_job = "Software Engineer"
+print("===============================================")
+print("                DIGITAL ID CARD                ")
+print("===============================================")
+print()
+print("Name        : ", name)
+print("Age         : ", age)
+print("Branch      : ", branch)
+print("City        : ", city)
+print("Dream job   : ", dream_job)
+print()
+print("===============================================")
+print("           END OF DIGITAL ID CARD              ")
+print("===============================================")
